0d0add35f7eab9d5621d62dc8244053ff18de4ef
hoseok <kihs1080@gmail.com>
2018-12-10 21:55:37 +0900

    d2 sample

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import configureStore from './store/index';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import reducer from './reducer/index';
+import App from './component/App'
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore(reducer,{
+	"todos" : [
+		{"id":1, "todo":"빨래하기", "complete":false},
+		{"id":2, "todo":"청소하기", "complete":false},
+		{"id":3, "todo":"공부하기", "complete":false}
+	]
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const render = () => {
+  ReactDOM.render(
+  	<Provider store={store}>
+	    <App/>
+    </Provider>,
+    document.getElementById('root')
+  )
+};
+
+store.subscribe(render);
+render();
