ccbf88a88f699aab406995345a6dca6afaf86949
hoseok <kihs1080@gmail.com>
2019-02-10 14:11:20 +0900

    add login

---
+++ hsd_react/src/components/Login.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react'; 
+import PropTypes from 'prop-types'; 
+const propTypes = { };
+const defaultProps = { };
+class Login extends Component { 
+    constructor(props) { 
+        super(props)
+    }
+    render() {
+        return(
+            <div>Login</div>
+        );
+    }
+}
+Login.propTypes = propTypes;
+Login.defaultProps = defaultProps
+export default Login;
